0aa75fb6f6a7f82540dff3d7c590bb041b636762
AnkitaSharma43 <ankita_sharma@softprodigy.com>
2023-10-26 11:20:12 +0530

    react task

--- src/Pages/TodoList.js
+++ src/Pages/TodoList.js
@@ -6,45 +6,26 @@ const TodoList = () => {
     const [data, setData] = useState('');
     const [result, setResult] = useState([]);
     const [showAlert, setShowAlert] = useState(false);
-    const [editTask, setEditTask] = useState(null);
 
-    const handleAddOrEdit = (e, taskId) => {
+    const handleClick = (e) => {
         e.preventDefault();
-
+        
         if (data.trim() === '') {
+            // If no data is entered, show an alert
             setShowAlert(true);
-            return;
-        }
-
-        if (taskId !== null) {
-            // If we are editing a task, update it
-            const updatedResult = result.map((task) => {
-                if (task.id === taskId) {
-                    return { id: task.id, list: data };
-                }
-                return task;
-            });
-            setResult(updatedResult);
         } else {
             const newTask = { id: new Date().getTime().toString(), list: data };
             setResult([...result, newTask]);
+            setData(''); // Clear the input field
+            // Hide the alert if it was previously shown
+            setShowAlert(false);
         }
-
-        setData('');
-        setShowAlert(false);
-        setEditTask(null); // Exit edit mode
-    };
-
-    const handleEdit = (taskId, taskContent) => {
-        setData(taskContent);
-        setEditTask(taskId);
     };
 
     const handleDelete = (taskId) => {
+        // Filter out the task with the specified taskId
         const updatedResult = result.filter((task) => task.id !== taskId);
         setResult(updatedResult);
-        setShowAlert(false);
-        setEditTask(null); // Exit edit mode if deleting the task
     };
 
     return (
@@ -56,55 +37,34 @@ const TodoList = () => {
                             value={data}
                             onChange={(e) => setData(e.target.value)}
                             type="text"
-                            placeholder="Add task..."
+                            placeholder='Add task...'
                         />
-                        <button  className ="position-btn" onClick={(e) => handleAddOrEdit(e, editTask)}>
-                            {editTask ? 'Save Task' : 'Add Task'}
-                        </button>
+                        <button className="position-btn" onClick={handleClick}>Add Task</button>
                     </div>
-
+ 
                     <div className="todo-content">
                         <ul>
-                            {result.map((task) => (
+                            {result.map((task, index) => (
                                 <li key={task.id}>
-                                    {editTask === task.id ? (
-                                        <input
-                                            type="text"
-                                            value={data}
-                                            onChange={(e) => setData(e.target.value)}
-                                        />
-                                    ) : (
-                                        task.list
-                                    )}
+                                    {task.list}
                                     <div className="user">
-                                        {editTask === task.id ? (
-                                            <button onClick={(e) => handleAddOrEdit(e, task.id)}>
-                                                Save
-                                            </button>
-                                        ) : (
-                                            <FaEdit
-                                                color="#28094d"
-                                                onClick={() => handleEdit(task.id, task.list)}
-                                            />
-                                        )}
-                                        <RiDeleteBin5Fill
-                                            color="#28094d"
-                                            onClick={() => handleDelete(task.id)}
-                                        />
+                                        <div>
+                                            <FaEdit color='#28094d' />
+                                        </div>
+                                        <div>
+                                            <RiDeleteBin5Fill color='#28094d' onClick={() => handleDelete(task.id)} />
+                                        </div>
                                     </div>
-                                </li>
+                                </li>         
                             ))}
                         </ul>
                     </div>
                 </div>
+                {showAlert && <div className='error-wrap'><h5  className="error-msg">
+                Please enter some data.
+                </h5></div>}
             </div>
-            {showAlert && (
-                <div className='error-wrap'>
-                    <h5 className="error-msg">
-                        Please enter some data.
-                    </h5>
-                </div>
-            )}
+           
         </>
     );
 };
